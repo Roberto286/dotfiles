435c757e43990f699e386bd726954e442e865a12
Roberto Saliola <roberto.saliola96@gmail.com>
2026-02-06 14:58:11 +0100

    add nvim starter config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,15 @@
+local defaults = { noremap = true, silent = true }
+local map = vim.keymap.set
+
+vim.opt.number = true
+vim.opt.statuscolumn = "%s  %l %r " 
+vim.g.mapleader = " "
+
+map("n", " ", "<Nop>", { silent = true, remap = false })
+map("i", "jj", "<esc>l", defaults)
+map('n', '<Leader>w', ':write<CR>')
+map('n', '<Leader>a', ':wqa<CR>')
+map('n', '<Leader>x', ':wq<CR>')
+map('n', '<Leader>q', ':q<CR>')
+map('n', '<Leader>e', ':Explore<CR>')
+
