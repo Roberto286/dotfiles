cab985e4c52f9bc30e9cd1e81af36540a14ac730
Roberto Saliola <roberto.saliola96@gmail.com>
2026-02-12 11:21:05 +0100

    add whichkey plugin and formatted config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,73 +1,87 @@
+
 -- Opzioni base --------------------------------------------------------------
 
-vim.opt.number = true                 -- Numeri di riga assoluti
-vim.opt.relativenumber = true         -- Numeri relativi per movimenti rapidi
-vim.opt.statuscolumn = "%s %l %r "    -- Colonna status minimale (segni + linea)
-vim.g.mapleader = " "                 -- Leader su Space
-vim.opt.showmatch = true	      -- Evidenzia la parentesi/apice di chiusura 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.statuscolumn = "%s %l %r "
+vim.g.mapleader = " "
+vim.opt.showmatch = true
 
-local defaults = { noremap = true, silent = true }
+-- Keymaps -------------------------------------------------------------------
+
 local map = vim.keymap.set
+local defaults = {noremap = true, silent = true}
+
+-- Helper per aggiungere desc senza ripetere defaults
+local function with_desc(desc)
+    return vim.tbl_extend("force", defaults, {desc = desc})
+end
 
 -- Evita che lo space faccia cose strane prima del leader
-map("n", " ", "<Nop>", { silent = true, remap = false })
+map("n", " ", "<Nop>", {silent = true, remap = false, desc = "Leader noop"})
 
 -- Escape ergonomico in insert
-map("i", "jj", "<esc>l", defaults)
+map("i", "jj", "<esc>l", with_desc("Escape insert mode"))
 
 -- Terminale
-map("n", "<Leader>t", ":belowright split | terminal<CR>i", defaults) -- Apre il terminale in orizzontale IN INSERT MODE	
--- Doppio ESC nel terminal per tornare in normal mode
-map("t", "<Esc>", [[<C-\><C-n>]], defaults)  
+map("n", "<Leader>t", ":belowright split | terminal<CR>i", with_desc("Apri terminale (split orizzontale)"))
+
+map("t", "<Esc>", [[<C-\><C-n>]], with_desc("Terminal → Normal mode"))
 
 -- Comandi rapidi file -------------------------------------------------------
 
-map('n', '<Leader>w', ':write<CR>', defaults)  -- Salva
-map('n', '<Leader>a', ':wqa<CR>', defaults)    -- Salva tutto e chiudi
-map('n', '<Leader>x', ':wq<CR>', defaults)     -- Salva e chiudi
-map('n', '<Leader>q', ':q<CR>', defaults)      -- Chiudi
-map('n', '<Leader>e', ':Explore<CR>', defaults)-- File explorer (netrw)
+map("n", "<Leader>w", ":write<CR>", with_desc("Salva file"))
+
+map("n", "<Leader>a", ":wqa<CR>", with_desc("Salva tutto e chiudi"))
+
+map("n", "<Leader>x", ":wq<CR>", with_desc("Salva e chiudi"))
+
+map("n", "<Leader>q", ":q<CR>", with_desc("Chiudi finestra"))
+
+map("n", "<Leader>e", ":Explore<CR>", with_desc("Apri file explorer (netrw)"))
 
 -- Clipboard di sistema ------------------------------------------------------
--- Queste sono le versioni "con apice" ( "+ ) mappate sul leader.
--- Separiamo universo Vim (registri interni) da universo sistema.
 
-map({ "n", "v" }, "<leader>y", '"+y', defaults) -- Yank verso clipboard globale
-map({ "n", "v" }, "<leader>d", '"+d', defaults) -- Delete/cut verso clipboard globale
-map({ "n", "v" }, "<leader>c", '"+c', defaults) -- Change verso clipboard globale
-map({ "n", "v" }, "<leader>p", '"+p', defaults) -- Paste verso clipboard globale
+map({"n", "v"}, "<leader>y", '"+y', with_desc("Yank → clipboard sistema"))
+
+map({"n", "v"}, "<leader>d", '"+d', with_desc("Delete → clipboard sistema"))
+
+map({"n", "v"}, "<leader>c", '"+c', with_desc("Change → clipboard sistema"))
+
+map({"n", "v"}, "<leader>p", '"+p', with_desc("Paste da clipboard sistema"))
 
 -- Disabilitazione frecce -----------------------------------------------------
--- Se premi le frecce, ti ricorda che esistono le vim motions.
 
 local function no_arrows_msg(direction, hint)
-  return function()
-    vim.notify(
-      "Usa le vim motions, non le frecce → prova '" .. hint .. "' per andare " .. direction,
-      vim.log.levels.INFO,
-      { title = "No arrows club" }
-    )
-  end
+    return function()
+        vim.notify(
+            "Usa le vim motions, non le frecce → prova '" .. hint .. "' per andare " .. direction,
+            vim.log.levels.INFO,
+            {title = "No arrows club"}
+        )
+    end
 end
 
--- Normal mode
-map("n", "<Up>",    no_arrows_msg("su",   "k"), { noremap = true })
-map("n", "<Down>",  no_arrows_msg("giù",  "j"), { noremap = true })
-map("n", "<Left>",  no_arrows_msg("a sinistra", "h"), { noremap = true })
-map("n", "<Right>", no_arrows_msg("a destra",   "l"), { noremap = true })
+map("n", "<Up>", no_arrows_msg("su", "k"), with_desc("Disabilita freccia ↑"))
 
--- Insert mode (ti riporta anche in normal mentalmente) per il momento commentato perché non mi ci trovo
---map("i", "<Up>",    no_arrows_msg("su",   "k"), { noremap = true })
---map("i", "<Down>",  no_arrows_msg("giù",  "j"), { noremap = true })
---map("i", "<Left>",  no_arrows_msg("a sinistra", "h"), { noremap = true })
---map("i", "<Right>", no_arrows_msg("a destra",   "l"), { noremap = true })
+map("n", "<Down>", no_arrows_msg("giù", "j"), with_desc("Disabilita freccia ↓"))
 
--- Plugins
-vim.pack.add({
-'https://github.com/nvim-mini/mini.pairs', -- Auto pair
-'https://github.com/folke/tokyonight.nvim', -- Tokyonight theme
-'https://github.com/lukas-reineke/indent-blankline.nvim'      -- Indentation guides
-})
-require('mini.pairs').setup()
+map("n", "<Left>", no_arrows_msg("a sinistra", "h"), with_desc("Disabilita freccia ←"))
+
+map("n", "<Right>", no_arrows_msg("a destra", "l"), with_desc("Disabilita freccia →"))
+
+-- Plugins -------------------------------------------------------------------
+
+vim.pack.add(
+    {
+        "https://github.com/nvim-mini/mini.pairs",
+        "https://github.com/folke/tokyonight.nvim",
+        "https://github.com/lukas-reineke/indent-blankline.nvim",
+        "https://github.com/folke/which-key.nvim"
+    }
+)
+
+require("mini.pairs").setup()
 vim.cmd.colorscheme("tokyonight")
-require('ibl').setup()
+require("ibl").setup()
+
